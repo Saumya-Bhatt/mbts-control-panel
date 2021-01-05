fce83734caf22fa87abe1b283d549208e2428416
Saumya Bhatt <saumya.bhatt106@gmail.com>
2021-01-05 17:16:15 +0530

    Connected Pyrebase for database authentication

--- Extra/Test/Trials/TEST_AUTH_DATA.py
+++ Extra/Test/Trials/TEST_AUTH_DATA.py
@@ -1,19 +1,12 @@
-from flask import Flask
+import os
+import ast
+import pyrebase
 from os.path import join, dirname
 from dotenv import load_dotenv
 
-import firebase_admin
-import pyrebase
-import ast
-import os
-
-
-
-
-
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
-certi = ast.literal_eval(os.environ["FIREBASE_CREDS"])
+
 PYREBASE_CONFIG = {
     "apiKey"           : os.getenv('FIREBASE_API_KEY'),
     "authDomain"       : os.getenv('AUTH_DOMAIN'),
@@ -22,22 +15,26 @@ PYREBASE_CONFIG = {
     "storageBucket"    : os.getenv('STORAGE_BUCKET'),
     "messagingSenderId": os.getenv('MESSAGING_SENDER_ID'),
     "appId"            : os.getenv('APP_ID'),
-    "serviceAccount"   : certi
-}
+    "serviceAccount"   : ast.literal_eval(os.getenv("FIREBASE_CREDS"))
+    }
+
+
+firebase = pyrebase.initialize_app(PYREBASE_CONFIG)
+auth = firebase.auth()
 
 email='saumi10600@gmail.com'
 password='ThisIsMyMBTS1'
 
-firebase = pyrebase.initialize_app(PYREBASE_CONFIG)
-auth = firebase.auth()
 user = auth.sign_in_with_email_and_password(email, password)
 db = firebase.database()
+# all_users = db.child("Reviews").get()
+# count = 0
+# for user in all_users.each():
+#     if count > 2:
+#         break
+#     print(user.key()) # Morty
+#     print(user.val()) # {name": "Mortimer 'Morty' Smith"}
+#     count += 1
 
-
-cred = firebase_admin.credentials.Certificate(certi)
-app_fb = firebase_admin.initialize_app(cred, {'storageBucket': os.getenv('STORAGE_BUCKET'),}, name='storage')
-app = Flask('__name__')
-app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
-
-
-from modules import routes
+review_id = [rev.key() for rev in db.child('Reviews').get().each() if rev.val()['ID'] == 102]
+print(review_id)
--- modules/routes.py
+++ modules/routes.py
@@ -1,31 +1,21 @@
-from flask import render_template, url_for, redirect, request, flash
-from modules.functions import upload_review, update_review
-from modules.frame import NavbarActive
+from flask import render_template, url_for, redirect, flash
+from modules.functions import DatabaseOperations
+from modules.frame import NavbarActive, return_node, GetData
 from modules.forms import ReviewForm
-from modules import app, firebase, app_fb
+from modules import app
 
-from firebase_admin import storage
+db_crud = DatabaseOperations()
+db_data = GetData()
+
+
+
 
 
 @app.route('/')
 def home():
-
-    if isinstance(firebase.get('Contact-Us',''),None):
-        general = {
-            'reviews' : len(firebase.get('Reviews','')),
-            'messages' : 0
-        } 
-    else:
-        general = {
-            'reviews' : len(firebase.get('Reviews','')),
-            'messages' : len(firebase.get('Contact-Us',''))
-        }
-    ig_count = sum(
-        i['Instagram'] == 1 for i in list(firebase.get('Reviews','').values())
-    )
     activate = NavbarActive()
     activate.set_home()
-    return render_template('home.html', info=general, ig_count=ig_count, activate=activate)
+    return render_template('home.html', info=db_data.get_numbers(), activate=activate)
 
 
 
@@ -33,9 +23,8 @@ def home():
 def entry():
 
     input_data = ReviewForm()
-
     if input_data.validate_on_submit():
-        upload_review(input_data, app_fb, firebase)
+        db_crud.upload_review(input_data)
         flash('New review entry was added to the database!')
         return redirect(url_for('table'))
 
@@ -48,121 +37,58 @@ def entry():
 @app.route('/Edit/<int:review_id>',methods=['GET','POST'])
 def update(review_id):
 
-    reviews = firebase.get('Reviews','')
+    update_id, data = return_node(review_id, 'Reviews', 'ID', nodeValue=True)[0]
     update_data = ReviewForm()
+    activate = NavbarActive()
 
-    data = {}
-    update_id = ''
-    for i in reviews:
-        if reviews[i]['ID'] == review_id:
-            data = reviews[i]
-            update_id = i
-        
     if update_data.validate_on_submit():
-        update_review(update_data, update_id, app_fb, firebase)
+        db_crud.update_review(update_data, update_id)
         flash(f'Movie ID {update_id} was succesfully updated')
         return redirect(url_for('table'))
-
-    activate = NavbarActive()
     return render_template('update.html', review=data, form=update_data, update_id=update_id, review_id=review_id, activate=activate)
 
 
 
 @app.route('/table')
 def table():
-
-    result = firebase.get('Reviews','')
-    res = list(result.values())
-
-    total = len(res)
-    res.reverse()
     activate = NavbarActive()
     activate.set_table()
+    return render_template('tables.html', reviews=db_data.get_reviews(), total=len(db_data.get_reviews()), activate=activate)
 
-    return render_template('tables.html', reviews=res, total=total, activate=activate)
 
 
 @app.route('/messages')
-def message():
-
-    messages = firebase.get('Contact-Us','')
-    try:
-        msg = list(messages.values())
-        total_msg = len(msg)
-        no_messages = True if total_msg == 0 else False
-        msg.reverse()
-    except:
-        msg = None
-        total_msg = 0
-        no_messages = True
-    
+def message():    
     activate = NavbarActive()
     activate.set_message()
-    return render_template('message.html', total=total_msg, messages=msg, no_msg=no_messages, activate=activate)
+    no_messages = (len(db_data.get_messages()) == 0)
+    return render_template('message.html', total=len(db_data.get_messages()), messages=db_data.get_messages(), no_msg=no_messages, activate=activate)
 
 
 
 @app.route('/delete_review/<int:review_id>',methods=['GET','POST'])
 def delete(review_id):
-
-    item = {}
-    all_rev = firebase.get('Reviews','')
-    for i in all_rev:
-        if all_rev[i]['ID'] == review_id:
-            item = {
-                'ID': i,
-                'Image': all_rev[i]['Poster']
-            }
-
-    bucket = storage.bucket(app=app_fb)
-    blob = bucket.blob(item['Image'])
-    blob.delete()
-    firebase.delete('Reviews/',item['ID'])
-    notif = 'Review ID : ' + item['ID'] + ' of movie number : ' + str(review_id) + ' was deleted'
-    flash(notif)
+    flash(db_crud.delete_review(review_id))
     return redirect(url_for('table'))
 
 
 
 @app.route('/delete_message',methods=['GET','POST'])
 def delete_msg():
-
-    message_id = ''
-    string = request.args.get('message_id')
-    string1 = string.replace('+',' ')
-    string2 = string1.replace('%3A',':')
-    string3 = string2.replace('%2F','/')
-
-    all_rev = firebase.get('Contact-Us','')
-    for i in all_rev:
-        if all_rev[i]['Subject'] == string3: 
-            message_id = i
-
-    firebase.delete('Contact-Us/',message_id)
-    notif = 'Message ID: ' + message_id + ' was deleted'
-    flash(notif)
+    flash(db_crud.delete_message())
     return redirect(url_for('message'))
+
 
 
 @app.route('/add_to_IG/<int:review_id>',methods=['GET','POST'])
 def IG_update(review_id):
-
-    item = ''
-    all_rev = firebase.get('Reviews','')
-    for i in all_rev:
-        if all_rev[i]['ID'] == review_id:
-            item = i
-
-    pathway = 'Reviews/' + str(item)
-    firebase.put(pathway, 'Instagram', 1)
-    
-    notif = 'Review ID : ' + item + ' was updated to Instagram'
-    flash(notif)
+    flash(db_crud.update_ig(review_id))
     return redirect(url_for('table'))
 
 
 
-#Handling error 404 and displaying relevant web page
+
+
 @app.errorhandler(404)
 def not_found_error(error):
     activate = NavbarActive()
@@ -170,7 +96,6 @@ def not_found_error(error):
     er_msg = 'This page probably does not exist or has been removed'
     return render_template('error.html', activate=activate, error=er, error_message=er_msg),404
  
-#Handling error 500 and displaying relevant web page
 @app.errorhandler(500)
 def internal_error(error):
     activate = NavbarActive()
